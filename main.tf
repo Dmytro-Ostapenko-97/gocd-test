cbd49e2dacc4113ef6ec1aee5562b39c8d84e1ce
Dmytro Ostapenko <d.ostapenko97@gmail.com>
2020-07-31 18:18:42 +0300

    dqdwdw

--- main.tf
+++ main.tf
@@ -3,86 +3,26 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "kubernetes_namespace" "test" {
+provider "kubernetes" {
+  host = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token = data.aws_eks_cluster_auth.cluster.token
+  load_config_file = false
+  version = "~> 1.9"
+}
+
+/*resource "kubernetes_namespace" "test" {
   metadata {
     name = "test"
     annotations = {
       name = "test"
     }
   }
-}
+}*/
 
-resource "aws_iam_role" "eks_worker_role" {
-  name  = "${var.global_prefix}-workers-role"
-  path  = "/"
-  tags  = {
-    Env = lower(var.env_name)
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy" "eks_worker_policy" {
-  name = "${var.global_prefix}-workers-role-policy"
-  role = aws_iam_role.eks_worker_role.id
-  lifecycle {
-    create_before_destroy = true
-  }
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-        "Effect":"Allow",
-        "Action":[
-          "ec2:*",
-          "iam:*",
-          "s3:*",
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:DescribeAutoScalingInstances",
-          "autoscaling:DescribeLaunchConfigurations",
-          "autoscaling:DescribeTags",
-          "autoscaling:SetDesiredCapacity",
-          "autoscaling:TerminateInstanceInAutoScalingGroup",
-          "ecr:*",
-          "elasticloadbalancing:RegisterTargets"
-        ],
-        "Resource":"*"
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy_attachment" "eks_worker_attach_node_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_worker_role.id
-}
-resource "aws_iam_role_policy_attachment" "eks_worker_attach_cni_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_worker_role.id
-}
-resource "aws_iam_role_policy_attachment" "eks_worker_attach_container_registry_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_worker_role.id
-}
 
-//module "ecr" {
-//  source = "git::https://github.com/greg-solutions/terraform_aws_ecr.git"
-//  image_tag = "gocd-test-derm"
-//  repository_prefix = "gocd-test-derm"
-//}
+module "ecr" {
+  source = "git::https://github.com/greg-solutions/terraform_aws_ecr.git"
+  image_tag = "gocd-test-derm"
+  repository_prefix = "gocd-test-derm"
+}
--- remote_state.tf
+++ remote_state.tf
@@ -6,3 +6,12 @@ data "terraform_remote_state" "base_remote_state" {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "kube_remote_state" {
+  backend = "s3"
+  config = {
+    bucket = "jackbot-greg-solution-terraform-state"
+    key = "kube-dev.tfstate"
+    region = "us-east-1"
+  }
+}
