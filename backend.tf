98da38802fa199118498f9653b32a012f12a088a
Dmytro Ostapenko <d.ostapenko97@gmail.com>
2020-08-01 14:50:05 +0300

    Add another steps

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "= 0.12.28"
   backend "s3" {
     bucket = "jackbot-greg-solution-terraform-state"
-    key = "gocd-test.tfstate"
+#    key = "gocd-test.tfstate"
     region = "us-east-1"
     //dynamodb_table = "terraform-remote-state-lock-db"
   }
