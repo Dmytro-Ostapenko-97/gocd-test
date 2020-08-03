30420297d46a0def944f73690c366b4c45444d98
Dmytro Ostapenko <d.ostapenko97@gmail.com>
2020-08-03 10:39:49 +0300

    try ecr

--- backend.tf
+++ backend.tf
@@ -7,3 +7,67 @@ terraform {
     //dynamodb_table = "terraform-remote-state-lock-db"
   }
 }
+
+
+XPBf3Md7MZwdts
+
+
+pipeline {
+  agent none
+
+  stages {
+    stage('Test') {
+      agent any
+      steps {
+        script {
+          sh '''
+		  echo ${STORE_CODE}'''
+        }
+      }
+    }
+	}
+}
+
+
+curl -d "STORE_CODE=test" -i -X POST https://dmytro_ostapenko:114a820a8760fda00155b7abb0d01ae157@jenkins.gregacademy.icu/job/test/buildWithParameters
+
+curl -d "token=XPBf3Md7MZwdts&STORE_CODE=Test" -i -X POST https://dmytro_ostapenko:114a820a8760fda00155b7abb0d01ae157@jenkins.gregacademy.icu/job/test/buildWithParameters
+
+curl -d STORE_CODE=test -i -X POST https://dmytro_ostapenko:114a820a8760fda00155b7abb0d01ae157@jenkins.gregacademy.icu/job/test/buildWithParameters?token=XPBf3Md7MZwdts
+
+
+curl -d "STORE_CODE=TEST1" -i -X POST https://dmytro_ostapenko:114a820a8760fda00155b7abb0d01ae157@jenkins.gregacademy.icu/job/test/buildWithParameters
+
+pipeline {
+  agent none
+
+  stages {
+    stage('Test') {
+      agent any
+      steps {
+        script {
+          sh '''
+		  echo ${STORE_CODE}'''
+        }
+        
+      dir("${env.WORKSPACE}/test_folder") {
+        sh '''
+        pwd'''
+      }
+      
+      }
+    }
+  }
+  
+}
+
+import jenkins.model.*
+
+def matchedJobs = Jenkins.instance.items.findAll { job ->
+    job.name =~ /${STORE_CODE}/
+}
+    
+matchedJobs.each { job ->
+    println job.name
+    job.delete()
+}
